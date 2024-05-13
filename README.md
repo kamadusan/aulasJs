05640cbba32ad3fadef358bfae3cdce1bb7cc61a
kamadusan <moontoon947@gmail.com>
2024-05-13 16:03:40 -0300

    aula 1 a 4 do curso

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # aulasJs
+- echo "# nomedodiretorio" >> README.md
+
+- git init
+
+- git add .
+
+- git commit -m "first commit"
+
+- git remote add origin https://github.com/'username'/nomedorepositorio.git
+
+- git push --set-upstream origin master
+
+- git push
--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-document.getElementById("texto").innerHTML
+// document.getElementById("texto").innerHTML = "meu primeiro texto <b>JS</b>!";
+
+// function minhafuncao(){
+//     document.write(5+5);
+//     document.write(5+5);
+//     document.write(5+5);
+// }
+
+// var let const são as variaveis do js
+var a=1;
+var b=5;
+var c=a+b;
+console.log(c);
+
+var pessoa = "Ad"
+var PESSOA = "random"
+console.log(pessoa);
+console.log(PESSOA);
+// o js é case sensitive
+676
